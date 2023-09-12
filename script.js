5fafbeb202246ebc7bf9ad9f13fff4e4b31ff42a
and96437345 <and96437345@yandex.ru>
2023-09-12 11:23:12 +0300

    JS_1 DZ

---
+++ script.js
@@ -0,0 +1,59 @@
+// Задание 1
+// let userName = prompt('Введите свое имя', '')
+// alert('Привет, ' + userName + '!')
+
+
+// Задание 2
+// const currentYear = 2023
+// let userAge = prompt('Введите год своего рождения', '')
+// alert('Вам ' + (currentYear - userAge) + ' лет')
+
+
+// Задание 3
+// let sideLength = prompt('Введите длину стороны квадрата', '')
+// alert('Периметр квадрата = ' + sideLength * 4)
+
+
+// Задание 4
+// const pi = 3.1415926
+// let squareRadius = prompt('Введите радиус окружности', '')
+// alert('Площадь окружности = ' + (pi * (squareRadius ** 2)))
+
+
+// Задание 5
+// let distance = prompt('Введите расстояние между городами в км', '')
+// let time = prompt('За сколько часов хотите добраться? ', '')
+// alert('Вам необходимо двигаться со скоростью ' + (distance / time) + ' км/ч')
+
+
+// Задание 6
+// const eurusd = 1.07
+// let usd = prompt('Введите количество долларов', '')
+// alert('Это будет ' + (usd / eurusd) + ' евро')
+
+
+// Задание 7
+// const fileVolume = .82
+// let flashVolume = prompt('Введите объем флешки в Гб', '')
+// alert('На флешку поместится ' + (flashVolume / fileVolume) + ' файлов размером 820 Мб')
+
+
+// Задание 8
+// let money = prompt('Введите сумму денег в кошельке, руб', '')
+// let price = prompt('Введите цену одной шоколадки, руб', '')
+// alert('Вы можете купить ' + (money / price) + ' шоколадок. Сдача ' + (money % price) + ' руб.')
+
+
+// Задание 9
+// let value = prompt('Введите трехзначное число', '')
+// let razr1 = String(value % 10)
+// let razr2 = String((value - razr1) / 10 % 10)
+// let razr3 = String((value - (value % 100)) / 100)
+// alert('Это число задом наперед будет ' + razr1 + razr2 + razr3)
+
+
+// Задание 10
+// let value = prompt('Введите число', '')
+// let res = (0 == (value % 2))
+// res || alert("Число нечетное")
+// !res || alert("Число четное")
